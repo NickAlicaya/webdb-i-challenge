104a6ef88eb8367febab0b31d12fc98f478baad7
nicanor-alicaya <donalicaya@gmail.com>
2020-01-21 19:06:16 -0500

    done

--- server.js
+++ server.js
@@ -1,9 +1,21 @@
 const express = require('express');
 
-const db = require('./data/dbConfig.js');
+const accountRouter = require("./accounts/accountRouter");
+
 
 const server = express();
 
 server.use(express.json());
+
+server.use("/api/accounts", accountRouter)
+
+server.get("/", (req,res) => {
+    return res.send("<h1>Server is Running on port 4000</h1>")
+})
+
+server.use((err,req,res,next) => {
+    console.log(err);
+    return res.status(500).json({error: "An error occured."})
+})
 
 module.exports = server;
